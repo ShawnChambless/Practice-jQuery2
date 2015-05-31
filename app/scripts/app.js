2f115ce8f8c87fc9c4a0514edd800336a14f69c9
Shawn Chambless <shawn.chambless@gmail.com>
2015-05-30 20:38:13 -0600

    Started over

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,67 +1,7 @@
 $(function() {
 
-    $('newTaskForm').hide();
+        var list = [];
 
-    var listo = [];
-
-    var Task = function(task) {
-        this.task = task;
-        this.id = 'new';
-    };
-
-    var addTask = function(task) {
-        if(task) {
-            task = new Task(task);
-            listo.push(task);
-
-            $('#newItemInput').val('');
-            $('#newList').append('<ahref="#" class="" id="item"<li class="list-group-item">' + task.task +'<span class="arrow pull-right"><i class="glyphicon glyphicon-arrow-right"></span></li></a>');
-        }
-
-        $('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
-
-    };
-
-    $('#saveNewItem').click(function(e) {
-        e.preventDefault();
-        var task = $('#newItemInput').val().trim();
-        addTask(task);
-    });
-
-    $('#newListItem').click(function() {
-        $('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
-    });
-
-    $('#cancel').click(function(e) {
-        e.preventDefault();
-        $('#newTaskForm, #newListItem').fadeToggle('fast', 'linear');
-    });
-
-    $(document).click('#item', function(e) {
-        e.preventDefault();
-        var task = this;
-        advanceTask(task);
-        this.id = 'inProgress';
-        $('#currentList').append(this.outerHTML);
-        localStorage.setItem('task', 'id');
-    });
-
-    $(document).click('#inProgress', function (e) {
-        e.preventDefault();
-        var task = this;
-        task.id = "archived";
-        var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
-        advanceTask(task);
-        $('#archivedList').append(changeIcon);
-    });
-
-    $(document).click('#archived', function(e) {
-        e.preventDefault();
-        var task = this;
-        advanceTask(task);
-    });
-
-
-
+        
 
 });
